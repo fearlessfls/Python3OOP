683931e58ec45ce06cb9f63c6eb21a09778916fb
Mr.Fearless <48866926+flessmcp@users.noreply.github.com>
2020-05-22 16:27:35 +0630

    Add files via upload

---
+++ 10.ducktyping.py
@@ -0,0 +1,10 @@
+class vscode:
+    def execute(self):
+        print("Compiling")
+        print("Generating Output")
+class Laptop:
+    def code(self,ide):
+        ide.execute()
+ide = vscode()
+lap1 = Laptop()
+lap1.code(ide)
